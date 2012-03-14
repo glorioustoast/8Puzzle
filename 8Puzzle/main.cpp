e17178b7d5923adc43ca316cd78c5398e8719ce0
Patrick Lindsay <glorious.toast@gmail.com>
2012-03-13 20:06:29 -0500

    Initial Commit

---
+++ 8Puzzle/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  8Puzzle
+//
+//  Created by Patrick Lindsay on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
